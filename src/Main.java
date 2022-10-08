33238aa8ac92e38fe589e242e32e87ecf4497ae5
JavaSenior <wecare2018s@gmail.com>
2022-10-08 16:34:28 +0200

    from pc

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hi");
+
+        //from pc
+
+        System.out.println("this message send from pc");
     }
 }
 
